b5f91f4e33d09ebf625f171264cc9529adc87912
Peter Moyer <peter.moyer@eagles.oc.edu>
2020-03-05 23:18:16 -0600

    Linked homepage buttons to webpages.

--- Homepage/urls.py
+++ Homepage/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    
+    # Go to Schedule Page
+    path('schedule/', views.schedule, name='schedule'),
+
+    # Go to thermostat Page
+    path('thermostat/', views.thermostat, name='thermostat'),
+
+    # Go to Water Info Page
+    path('water_info/', views.water_info, name='water_info'),
+
+    # Go to Homepage
+    path('', views.home, name='home'),
 ]
--- Homepage/views.py
+++ Homepage/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def index(request):
+def home(request):
     return render(request, 'home.html')
+
+def schedule(request):
+#    return render(request, 'schedule.html')
+    return HttpResponse('Welcome to the Schedule Page')
+
+def thermostat(request):
+#    return render(request, 'thermostat.html')
+    return HttpResponse('Welcome to the Thermostat Page')
+
+def water_info(request):
+#    return render(request, 'water_info.html')
+    return HttpResponse('Welcome to the Water Info Page')
