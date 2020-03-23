2619dadde3268577d83fe1042b5b6fcb75203021
Marcus Mellor <marcus.mellor@eagles.oc.edu>
2020-03-23 10:52:15 -0500

    Switched to dark theme

--- Homepage/views.py
+++ Homepage/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', context={ "meta" : {"title" : "Goober Pool Control"} })
